411b1bbe8ead65f1c3c7f677aaf8fc231a29c3ab
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2026-07-09 15:11:14 -0600

    Refactor correctCredentials function to include error handling

--- AutomatingFormSubmission/automateForm.py
+++ AutomatingFormSubmission/automateForm.py
@@ -22,25 +22,28 @@ def wrongCredentials(driver):
 
 
 def correctCredentials(driver):
-    #Find the username field using XPATH and enter the correct username
-    usernameField=driver.find_element(by=By.XPATH, value="//*[@id='username']")
-    usernameField.send_keys("tomsmith")
-    time.sleep(1)
-    #Find the password field using XPATH and enter the correct password
-    passwordField=driver.find_element(by=By.XPATH, value="//*[@id='password']")
-    passwordField.send_keys("SuperSecretPassword!")
-    time.sleep(1)
-    #Find the login button using XPATH and click it
-    loginButton=driver.find_element(by=By.XPATH, value="//*[@id='login']/button")
-    loginButton.click()
-    time.sleep(2)
-    #Find the logout button using XPATH and click it
-    logoutButton=driver.find_element(by=By.XPATH, value="//*[@id='content']/div/a")
-    logoutButton.click()
-    time.sleep(2)
+    try:
+        #Find the username field using XPATH and enter the correct username
+        usernameField=driver.find_element(by=By.XPATH, value="//*[@id='username']")
+        usernameField.send_keys("tomsmith")
+        time.sleep(1)
+        #Find the password field using XPATH and enter the correct password
+        passwordField=driver.find_element(by=By.XPATH, value="//*[@id='password']")
+        passwordField.send_keys("SuperSecretPassword!")
+        time.sleep(1)
+        #Find the login button using XPATH and click it
+        loginButton=driver.find_element(by=By.XPATH, value="//*[@id='login']/button")
+        loginButton.click()
+        time.sleep(2)
+        #Find the logout button using XPATH and click it
+        logoutButton=driver.find_element(by=By.XPATH, value="//*[@id='content']/div/a")
+        logoutButton.click()
+        time.sleep(2)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-def main():
+def main(option):
     driver = webdriver.Edge()
     driver.get("https://the-internet.herokuapp.com/login")
     
@@ -58,4 +61,4 @@ def main():
 
 if __name__ == "__main__":
     option = 'correct'  # 'wrong' or 'correct'
-    main()
+    main(option)
